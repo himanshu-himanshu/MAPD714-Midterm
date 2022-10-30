fd475db3c7146db0b408fe6b22ba04e2887dcc63
Himanshu <himanshu16315@gmail.com>
2022-10-29 22:13:09 -0400

    Added functionality for steppers and some style changes

--- MAPD714-Midterm/ShoppingListViewController.swift
+++ MAPD714-Midterm/ShoppingListViewController.swift
@@ -12,19 +12,19 @@ class ShoppingListViewController: UIViewController {
     @IBOutlet weak var listNameField: UITextField!
     
     
-    @IBOutlet weak var listItem1: UITextField!
+    @IBOutlet weak var item1: UITextField!
     
     
-    @IBOutlet weak var listItem2: UITextField!
+    @IBOutlet weak var item2: UITextField!
     
     
-    @IBOutlet weak var listItem3: UITextField!
+    @IBOutlet weak var item3: UITextField!
     
     
-    @IBOutlet weak var listItem4: UITextField!
+    @IBOutlet weak var item4: UITextField!
     
     
-    @IBOutlet weak var listItem5: UITextField!
+    @IBOutlet weak var item5: UITextField!
     
     
     @IBOutlet weak var quantity1: UILabel!
@@ -41,42 +41,73 @@ class ShoppingListViewController: UIViewController {
     
     @IBOutlet weak var quantity5: UILabel!
     
-    
+
     @IBAction func stepper1(_ sender: UIStepper) {
-       quantity1.text = String(Int(sender.value))
+        quantity1.text = String(Int(sender.value))
     }
     
     @IBAction func stepper2(_ sender: UIStepper) {
+        quantity2.text = String(Int(sender.value))
     }
     
     @IBAction func stepper3(_ sender: UIStepper) {
+        quantity3.text = String(Int(sender.value))
     }
     
     @IBAction func stepper4(_ sender: UIStepper) {
+        quantity4.text = String(Int(sender.value))
     }
     
+    
     @IBAction func stepper5(_ sender: UIStepper) {
+        quantity5.text = String(Int(sender.value))
     }
+    
+    func resetQuantity() {
+        quantity1.text = "1"
+        quantity2.text = "1"
+        quantity3.text = "1"
+        quantity4.text = "1"
+        quantity5.text = "1"
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        quantity1.text = "0"
-        quantity2.text = "0"
-        quantity3.text = "0"
-        quantity4.text = "0"
-        quantity5.text = "0"
+        resetQuantity()
+    }
+    
+    func showAlert() {
+        let alertController = UIAlertController(title: "Error", message:
+            "Fields Cannot be empty!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+
+        self.present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func saveButton(_ sender: UIButton) {
+        if(item1.text == "" || item2.text == "" || item3.text == "" || item4.text == "" || item5.text == "") {
+            return showAlert()
+        }
         listName =  (listNameField.text == "" ? "Shopping List" : listNameField.text)!
         print(listName)
+        print(item1.text!, "=>", quantity1.text!)
+        print(item2.text!, "=>", quantity2.text!)
+        print(item3.text!, "=>", quantity3.text!)
+        print(item4.text!, "=>", quantity4.text!)
+        print(item5.text!, "=>", quantity5.text!)
     }
     
     @IBAction func cancelButton(_ sender: UIButton) {
         listNameField.text = ""
         listName = ""
+        item1.text = ""
+        item2.text = ""
+        item3.text = ""
+        item4.text = ""
+        item5.text = ""
+        resetQuantity()
     }
-    
     
 }
 
